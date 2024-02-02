bd5ce2f2bbdb976cffa5cceefb8ba951f0a2c5e9
Antonio <antonio@Antonios-MacBook-Pro-2.local>
2024-02-01 20:02:44 -0500

    connected UIlabel on storyboard to instance on VC for questionlabel and answerlabel git status

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
