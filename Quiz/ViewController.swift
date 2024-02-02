944b3175f6838655088e51e4752195b188207413
Antonio <antonio@Antonios-MacBook-Pro-2.local>
2024-02-02 17:50:02 -0500

    Created IBAction func for showNextQuestion and ShowAnswer, When nextQuestion button is clicked it indexs through a fixed answerArray and showAnswerArray, currentQuestionIndex rests once index array == questionArray count

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,7 +12,34 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = [
+    "what is 7+7?", "What is the captial of vermont", "What is cognac made from?"
+    ]
     
+    let answers: [String] = ["14","Montpeliar","Grapes"]
+
+    var currentQuestionsIndex: Int = 0
+    
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionsIndex += 1
+        
+        if currentQuestionsIndex == questions.count {
+            currentQuestionsIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionsIndex]
+        
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionsIndex]
+    
+        answerLabel.text = answer
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
